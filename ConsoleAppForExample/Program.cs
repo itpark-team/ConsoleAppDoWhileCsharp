9851becaf7aab4e576d7fe1a150ef28991437853
itpark <a.m.smyslov@yandex.ru>
2022-11-27 15:02:28 +0300

    done cycles

--- ConsoleAppForExample/Program.cs
+++ ConsoleAppForExample/Program.cs
@@ -1,24 +1,36 @@
-﻿/*int i = 0;
-while (i < 5)
+﻿// int i = 0;
+// while (i > 0)
+// {
+//     Console.WriteLine("hello world");
+//     i-=2;
+// }
+// Console.WriteLine("finish");
+
+for (int i = 0; i > 0; i--)
 {
     Console.WriteLine("hello world");
-    i++;
-}*/
-
-for (int i = 0; i < 5; i++)
-{
-    Console.WriteLine("hello world");
 }
+Console.WriteLine("finish");
 
-for (int i = 0; i < 15; i++)
-{
-    Console.WriteLine("world hello");
-}
+// for (int i = 0; i < 5; i++)
+// {
+//     Console.WriteLine("hello world");
+// }
+//
+// for (int i = 0; i < 15; i++)
+// {
+//     Console.WriteLine("world hello");
+// }
+//
+// for (int i = 0; i < 3; i++)
+// {
+//     for (int j = 0; j < 3; j++)
+//     {
+//         
+//     }
+// }
 
-for (int i = 0; i < 3; i++)
-{
-    for (int j = 0; j < 3; j++)
-    {
-        
-    }
-}
+// while (true)
+// {
+//     
+// }

b13092ec7a5d06862b015b04afb38a8109431fa0
itpark <a.m.smyslov@yandex.ru>
2022-11-27 14:57:29 +0300

    Initial commit

---
+++ ConsoleAppDoWhileCsharp/Program.cs
@@ -0,0 +1,24 @@
+﻿double number;
+
+do
+{
+    try
+    {
+        Console.Write("введите число >0 для извлечения кв корня: ");
+        number = double.Parse(Console.ReadLine());
+        if (number < 0)
+        {
+            Console.WriteLine("ошибка. число должно быть >0");
+        }
+    }
+    catch
+    {
+        Console.WriteLine("ошибка. ввод должен быть числом");
+        number = -1;
+    }
+} while (number < 0);
+
+
+double res = Math.Sqrt(number);
+
+Console.WriteLine($"Квадратный корень из {number} = {res}");
---
+++ ConsoleAppForExample/Program.cs
@@ -0,0 +1,24 @@
+﻿/*int i = 0;
+while (i < 5)
+{
+    Console.WriteLine("hello world");
+    i++;
+}*/
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("hello world");
+}
+
+for (int i = 0; i < 15; i++)
+{
+    Console.WriteLine("world hello");
+}
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        
+    }
+}
